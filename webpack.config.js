a520d3c1367005e507774889365a6d54d4c870a8
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-23 22:55:43 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
+            "plugins": ["@babel/plugin-transform-runtime"],
           },
         },
       },
