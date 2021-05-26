bf8a4a12f7046070706246af113840391f6abb13
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-26 01:54:24 -0400

    updated box logic

--- src/Components/Box/Box.js
+++ src/Components/Box/Box.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
+import numeral from "numeral";
 import "./box.css";
 
 // destructure props into variables
 
 // (props) props.title, props.cases, props.total etc
-function Box({ title, cases, active, isRed, total, ...props }) {
+function Box({ title, cases, active, total, ...props }) {
   return (
     <Card
     onClick={props.onClick}
-      className={`infoBox ${active && "infoBox--selected"} ${
-        isRed && "infoBox--red"
-      }`}
+      className={`infoBox ${active && "infoBox--selected"}`}
     >
       <CardContent>
         <Typography className="infoBox__title" color="textSecondary">
           {title}
         </Typography>
 
-        <h2 className="infoBox__cases">{cases}</h2>
+        <h2 className="infoBox__cases">{numeral(cases).format("+0,0")}</h2>
 
         <Typography className="infoBox__total" color="textSecondary">
-          {total} Total
+          {numeral(total).format("0,0")} Total
         </Typography>
       </CardContent>
     </Card>
