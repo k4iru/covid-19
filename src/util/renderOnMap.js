56c1dce6a8c1e5a7de821b5de88ddbe870de2fa5
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-26 01:53:36 -0400

    updated multipliers

--- src/util/renderOnMap.js
+++ src/util/renderOnMap.js
@@ -7,25 +7,26 @@ const casesTypeColors = {
     hex: "#CC1034",
     rgb: "rgb(204, 16, 52)",
     half_op: "rgba(204, 16, 52, 0.5)",
-    multiplier: 800,
+    multiplier: 300,
   },
   recovered: {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
     half_op: "rgba(125, 215, 29, 0.5)",
-    multiplier: 1200,
+    multiplier: 500,
   },
   deaths: {
     hex: "#fb4443",
     rgb: "rgb(251, 68, 67)",
     half_op: "rgba(251, 68, 67, 0.5)",
-    multiplier: 2000,
+    multiplier: 700,
   },
 };
 
 export const renderOnMap = (data, casesType = "cases") =>
   data.map((country) => (
-    <Circle
+
+    <Circle key={country.country}
       center={[country.countryInfo.lat, country.countryInfo.long]}
       fillOpacity={0.1}
       color={casesTypeColors[casesType].hex}
