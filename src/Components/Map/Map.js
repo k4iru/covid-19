0de2d0af936b9ad7a27abe3c7e761d7039307a18
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-26 02:12:00 -0400

    removed zoom lock

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -7,7 +7,7 @@ function Map({ countries, caseType, center, zoom }) {
   
   return (
     <div className="map">
-      <LeafletMap center={center} zoom={zoom} scrollWheelZoom={false} >
+      <LeafletMap center={center} zoom={zoom} >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
